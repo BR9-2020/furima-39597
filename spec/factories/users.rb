2b7d824c4846fa7bc019ece8113e3e6a5f7bd88f
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-09-07 18:32:06 +0900

    FactoryBotのuserモデルにFakerを使用

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
   factory :user do
-    nickname                   {'test'}
-    email                      {'test@example'}
-    password                   {'000000a'}
-    password_confirmation      {password}
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.email}
+    last_name             {Faker::Name.last_name}
+    first_name            {Faker::Name.first_name}
+
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,15 +6,41 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it '* nicknameとemail、last_nameとfirst_name、last_name_kanaとfirst_name_kana、birth_date、passwordとpassword_confirmationが存在すれば登録できる' do
+    end
     it 'nicknameが空では登録できない' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'last_nameが空では登録できない' do
+    end
+    it 'first_nameが空では登録できない' do
+    end
+    it 'last_name_kanaが空では登録できない' do
+    end
+    it 'first_name_kanaが空では登録できない' do
+    end
+    it 'last_name_kanaはカタカナしか登録できない' do
+    end
+    it 'first_name_kanaはカタカナしか登録できない' do
+    end
+    it 'birth_dateが空では登録できない' do
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+    end
+    it '重複したemailが存在する場合は登録できない' do
+    end
+    it 'emailは@を含まないと登録できない' do
+    end
+    it 'passwordが5文字以下では登録できない' do
+    end
+    it 'passwordが129文字以上では登録できない' do
+    end
+    it 'passwordが数字だけでは登録できない' do
+    end
+    it 'passwordが英字だけでは登録できない' do
     end
   end
 end
