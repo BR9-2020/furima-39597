a504cd0f99237bf026576bf54e0379286859737b
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-09-13 17:21:47 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :shipping_region_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
+
 end
