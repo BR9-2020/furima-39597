18e511c1a60b4c303cf0b95c73b44e454b767182
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-09-14 12:42:15 +0900

    カテゴリー機能の導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :shipping_region
+  belongs_to :shipping_days
+
   belongs_to :user
   has_one_attached :image
   validates :image, presence: true
@@ -10,5 +17,6 @@ class Item < ApplicationRecord
   validates :shipping_region_id, presence: true
   validates :shipping_day_id, presence: true
   validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
