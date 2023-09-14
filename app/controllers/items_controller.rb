573a6d8a6e2f6b50d628ffd35e50f1b54b5ae5de
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-09-14 11:14:18 +0900

    itemコントローラーの商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,16 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    prams.require(:item).permit(:image, :item_name, :item_description, :category_id, :condition_id, :shipping_cost_id, :shipping_region_id, :shipping_day_id, :price)
   end
 end
