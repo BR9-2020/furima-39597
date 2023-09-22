e496b4ef34d40d90e4d5e499c9e2989f6293921c
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-09-22 12:40:51 +0900

    商品を登録できる前までのテストコード実装完了

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name          {Faker::Vehicle.manufacture}
+    item_description   {Faker::Vehicle.version}
+    category_id        {Faker::Number.between(from: 2, to: 11)}
+    condition_id       {Faker::Number.between(from: 2, to: 7)}
+    shipping_cost_id   {Faker::Number.between(from: 2, to: 3)}
+    shipping_region_id {Faker::Number.between(from: 2, to: 48)}
+    shipping_day_id    {Faker::Number.between(from: 2, to: 4)}
+    price              {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/f1_car.png'), filename: 'f1_car.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user: @user)
+  end
+
+  describe '商品の出品登録' do
+    context '出品登録ができるとき' do
+      it 'カテゴリーが「---」以外であれば登録できる' do
+        @item.category_id = 2
+        expect(@item).to be_valid
+      end
+      it '商品の状態が「---」以外であれば登録できる' do
+        @item.condition_id = 2
+        expect(@item).to be_valid
+      end
+      it '配送料の負担が「---」以外であれば登録できる' do
+        @item.shipping_cost_id = 2
+        expect(@item).to be_valid
+      end
+      it '発送元の地域が「---」以外であれば登録できる' do
+        @item.shipping_region_id = 2
+        expect(@item).to be_valid
+      end
+      it '発送までの日数が「---」以外であれば登録できる' do
+        @item.shipping_day_id = 2
+        expect(@item).to be_valid
+      end
+      it '価格が半角数字で300円〜9,999,999円であれば登録できる' do
+        @item.price = 10000
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品ができないとき' do
+      it 'ユーザー登録している人以外は出品できない' do
+        @item.user_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User can't be blank")
+      end
+      it '画像がないと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空だと出品できない' do
+        @item.item_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it '商品の説明が空だと出品できない' do
+        @item.item_description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description can't be blank")
+      end
+      it 'カテゴリーの情報が「---」だと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態の情報が「---」だと出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料の負担の情報が「---」だと出品できない' do
+        @item.shipping_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+      end
+      it '発送元の地域の情報が「---」だと出品できない' do
+        @item.shipping_region_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping region can't be blank")
+      end
+      it '発送までの日数の情報が「---」だと出品できない' do
+        @item.shipping_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it '価格が空欄だと出品できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
+      end
+      it '価格が300円未満だと出品できない' do
+        @item.price = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '価格が9999999円を超えると出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
