84e450b9e4f8cbfde911703555bfa8fb4028d7d4
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-09-26 16:37:58 +0900

    itemモデルのuserバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,6 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  validates :user_id, presence: true
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_description, presence: true
