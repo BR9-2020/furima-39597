9bec970b24b1c6aa17313adb67701a783148ac46
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-09-26 17:18:23 +0900

    テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Item, type: :model do
       it 'ユーザー登録している人以外は出品できない' do
         @item.user_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User can't be blank")
+        expect(@item.errors.full_messages).to include("User must exist")
       end
       it '画像がないと出品できない' do
         @item.image = nil
@@ -94,6 +94,11 @@ RSpec.describe Item, type: :model do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it '価格に半角数字以外が含まれている場合は出品できない' do
+        @item.price = '８８８８'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
