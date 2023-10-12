a8f34f8246c1e794ceeecc9d21eb625cbaff70b6
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-10-12 10:03:31 +0900

    Formオブジェクトの作成

---
+++ app/models/purchase_shipment.rb
@@ -0,0 +1,19 @@
+class PurchaseShipment
+  include ActiveModel::Model
+  attr_accessor :postcode, :shipping_region_id, :city_town_village, :street_address, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :shipping_region_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :city_town_village
+    validates :street_address
+    validates :phone_number, {numericality: true, length: {in: 10..11}, message: 'is invalid'}
+    validates :user_id
+    validates :item_id
+  end
+
+  def save
+    purchase = Purcahse.create(user_id: user_id, item_id: item_id)
+    Shipment.create(postcode: postcode, shipping_region_id: shipping_region_id, city_town_village: city_town_village, street_address: street_address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
