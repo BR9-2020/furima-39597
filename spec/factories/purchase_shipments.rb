1969763fcb66060ebeaad1245aff92eebd6ba2eb
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-10-16 18:58:04 +0900

    商品購入機能のテストコード実装

---
+++ spec/factories/purchase_shipments.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :purchase_shipment do
+    postcode { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    shipping_region_id { Faker::Number.between(from: 2, to: 48) }
+    city_town_village { Faker::Address.city }
+    street_address { Faker::Address.street_address }
+    building_name { Faker::Address.secondary_address }
+    phone_number { Faker::Number.decimal_part(digits: 11) }
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+  end
+end
