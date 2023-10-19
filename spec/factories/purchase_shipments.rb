2fc1df93adce233890c6ad315c17ffbd7680afad
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-10-19 20:00:01 +0900

    FactoryBotの修正

--- spec/factories/purchase_shipments.rb
+++ spec/factories/purchase_shipments.rb
@@ -6,8 +6,6 @@ FactoryBot.define do
     street_address { Faker::Address.street_address }
     building_name { Faker::Address.secondary_address }
     phone_number { Faker::Number.decimal_part(digits: 11) }
-    user_id { Faker::Number.non_zero_digit }
-    item_id { Faker::Number.non_zero_digit }
     token { Faker::Internet.password(min_length: 20, max_length: 30) }
   end
 end
--- spec/models/purchase_shipment_spec.rb
+++ spec/models/purchase_shipment_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PurchaseShipment, type: :model do
   before do
-    @purchase_shipment = FactoryBot.build(:purchase_shipment)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @purchase_shipment = FactoryBot.build(:purchase_shipment,user_id: @user.id, item_id: @item.id)
+    sleep 0.1
   end
   describe '発送先情報の登録' do
     context '発送先情報が登録できる時' do
