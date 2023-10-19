9becfd7c2ec90158233786e2a2d640bb96417cb6
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-10-19 18:32:37 +0900

    商品購入機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -11,6 +11,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_shipment = PurchaseShipment.new(purchase_params)
     if @purchase_shipment.valid?
+      pay_item
       @purchase_shipment.save
       redirect_to root_path
     else
@@ -21,7 +22,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_shipment).permit(:postcode, :shipping_region_id, :city_town_village, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_shipment).permit(:postcode, :shipping_region_id, :city_town_village, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def find_item
@@ -29,8 +30,8 @@ class PurchasesController < ApplicationController
   end
 
   def check_seller
-    if current_user.id == @item.user_id # 出品者とログインユーザーが同じなら
-      redirect_to root_path # トップページへリダイレクト
+    if current_user.id == @item.user_id
+      redirect_to root_path 
     end
   end
 
@@ -38,5 +39,14 @@ class PurchasesController < ApplicationController
     if current_user.id == @item.user_id || @item.purchase.present? # ログインユーザーが出品者か、または商品が売り切れている場合
       redirect_to root_path # トップページにリダイレクト
     end
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_1afdaa97c0f54dedbc5f4c6a"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/purchase_shipment.rb
+++ app/models/purchase_shipment.rb
@@ -1,6 +1,6 @@
 class PurchaseShipment
   include ActiveModel::Model
-  attr_accessor :postcode, :shipping_region_id, :city_town_village, :street_address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postcode, :shipping_region_id, :city_town_village, :street_address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,6 +10,7 @@ class PurchaseShipment
     validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid"}
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
