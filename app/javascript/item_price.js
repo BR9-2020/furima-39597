8ffcf0bb0a2a291e84304c229c1d3801f3c6788f
BR9-2020 <rstoyo.riki.yoshida@gmail.com>
2023-10-20 10:40:30 +0900

    手数料と利益表示の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,15 @@
-window.addEventListener('turbo:load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
+const priceInput = document.getElementById("item-price");
+const addTaxDom = document.getElementById("add-tax-price");
+const ProfitDom = document.getElementById("profit");
+
+priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
   addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  const ProfitDom = document.getElementById("profit");
   ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
-  })
+});
+
+document.addEventListener('turbo:frame-load', (event) => {
+  if (event.target.id === 'item-price') {
+    priceInput.dispatchEvent(new Event('input'));
+  }
 });
